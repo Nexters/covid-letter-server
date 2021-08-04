8f18ec856fc988340d37842927a364bf11a57a90
623nana <6230nana@gmail.com>
2021-08-04 22:05:43 +0900

    test: QuestionRepository 질문 리스트 조회 테스트 #3

---
+++ src/test/java/com/nexters/covid/letter/domain/QuestionRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.nexters.covid.letter.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class QuestionRepositoryTest {
+
+  @Autowired
+  QuestionRepository questionRepository;
+
+  @Test
+  @DisplayName("[QuestionRepository] 선택한 옵션에 따른 질문과 공통 질문 조회 테스트")
+  void findQuestionByOptionTest() {
+    List<Question> questions = questionRepository.findQuestionsBySendOptionIdEqualsOrSendOptionIdEquals(1L, 3L);
+
+    List<Long> sendOptionIds = questions.stream()
+        .map(q -> q.getSendOption().getId())
+        .collect(Collectors.toList());
+
+    assertThat(sendOptionIds.contains(1L)).isTrue();
+    assertThat(sendOptionIds.contains(3L)).isTrue();
+  }
+}
