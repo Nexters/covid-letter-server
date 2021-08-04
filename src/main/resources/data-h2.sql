3bdb5aa259a53b27cea6cf4c3644308719905ec0
623nana <6230nana@gmail.com>
2021-08-04 10:54:48 +0900

    feat: 테스트 데이터 추가 #3

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -7,6 +7,7 @@ values ('ANSWER1', 'CONTENTS1', 'ENCRYPTED1', 'LETTER_TO1', 'email', 'BLUE', 2, 
 
 insert into send_option (text, covid_stat) values ('테스트', 1000);
 insert into send_option (text, covid_stat) values ('테스트2', 2000);
+insert into send_option (text, covid_stat) values ('기본질문', 0);
 
 insert into question (text, send_option_id) values ('테스트1에 1', 1);
 insert into question (text, send_option_id) values ('테스트1에 2', 1);
@@ -14,3 +15,7 @@ insert into question (text, send_option_id) values ('테스트1에 3', 1);
 
 insert into question (text, send_option_id) values ('테스트2에 1', 2);
 insert into question (text, send_option_id) values ('테스트2에 2', 2);
+
+insert into question (text, send_option_id) values ('기본 질문1', 3);
+insert into question (text, send_option_id) values ('기본 질문2', 3);
+insert into question (text, send_option_id) values ('기본 질문3', 3);
