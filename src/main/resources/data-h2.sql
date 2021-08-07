e3df571b8ab8da1bbb2f140ee6eb192a1c719851
623nana <6230nana@gmail.com>
2021-08-07 21:10:38 +0900

    test: 테스트 데이터 변경에 따른 테스트 수정 #3

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,9 +1,15 @@
 insert into user (email, identifier, name) values ('email', 'identifier', 'name');
+insert into user (email, identifier, name) values ('test@gmail.com', '1', '김아무개');
 
 insert into letter (contents, encrypted_id, letter_to, email, sticker, question_id, state, title, user_id)
 values ('CONTENTS', 'ENCRYPTED', 'LETTER_TO', 'email', 'HAPPY', 1 , 'PENDING', 'TITLE', 1);
 insert into letter (contents, encrypted_id, letter_to, email, sticker, question_id, state, title, user_id)
 values ('CONTENTS1', 'ENCRYPTED1', 'LETTER_TO1', 'email', 'BLUE', 2, 'PENDING', 'TITLE1', 1);
+
+insert into letter (contents, encrypted_id, letter_to, email, sticker, question_id, state, title, user_id)
+values ('CONTENTS2', 'ENCRYPTED2', 'LETTER_TO2', 'test@gmail.com', 'BLUE', 2, 'PENDING', 'TITLE2', 2);
+insert into letter (contents, encrypted_id, letter_to, email, sticker, question_id, state, title, user_id)
+values ('CONTENTS3', 'ENCRYPTED3', 'LETTER_TO3', 'test@gmail.com', 'BLUE', 2, 'PENDING', 'TITLE3', 2);
 
 insert into send_option (text, covid_stat) values ('테스트', 1000);
 insert into send_option (text, covid_stat) values ('테스트2', 2000);
